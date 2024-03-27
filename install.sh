4b6b29ea859c8afab45e7ed019bbdf44f3ba3f6e
cyxSetyo <98861415+cyxSetyo@users.noreply.github.com>
2024-03-27 14:22:24 +0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+Write-Host "Hello The AVOLife!"
+Read-Host "Press any key to continue.."
+
+Set-ExecutionPolicy Bypass -Scope Process
+
+<#
+.Install Chocolatey
+#>
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco -?
+
+<#
+.Aplikasi Google
+#>
+choco install googlechrome -y
+choco install google-drive-file-stream -y
+choco install drawio -y
+
+<#
+.Nice to Have
+#>
+choco install anydesk -y
+choco install zoom -y
+choco install winrar -y
+choco install vlc -y
